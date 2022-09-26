b35600d8471d47a7350f85a67d90ce6832b6a4bf
coboy <416212316@qq.com>
2022-09-26 16:12:59 +0800

    init

---
+++ packages/ast/test.js
@@ -0,0 +1,20 @@
+const esprima = require('esprima')
+const estraverse = require('estraverse')
+const escodegen = require('escodegen')
+
+const code = 'function fn() {}'
+
+let ast = esprima.parseScript(code)
+
+estraverse.traverse(ast, {
+    enter(node) {
+        if(node.type === 'FunctionDeclaration') {
+            node.id.name = 'Fn'
+        }
+    },
+    leave(node) {
+
+    }
+})
+
+console.log(escodegen.generate(ast))
